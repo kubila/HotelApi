8eb71d29bd72d817a0048583605c1b61808af011
kubila <dolaskyasin@gmail.com>
2021-02-06 22:25:15 +0300

    Add ConnectionString

--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,9 @@
+using HotelApi.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -28,6 +30,11 @@ namespace HotelApi
         {
 
             services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(c =>
+            {
+                c.UseSqlServer(Configuration.GetConnectionString("Default"));
+            });
 
             // configure cross origin resource sharing
             services.AddCors(crs =>
