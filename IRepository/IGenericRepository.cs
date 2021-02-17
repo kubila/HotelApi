5c112b990a5f68da7a464fc334f6d8cc7815db76
kubila <dolaskyasin@gmail.com>
2021-02-17 22:18:32 +0300

    Create a generic interface for Repository Pattern

---
+++ IRepository/IGenericRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelApi.IRepository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null);
+
+        Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null);
+
+        Task Insert(T item);
+
+        Task InsertRange(IEnumerable<T> items);
+
+        Task Delete(int id);
+
+        void DeleteRange(IEnumerable<T> items);
+
+        void Update(T item);
+    }
+}
