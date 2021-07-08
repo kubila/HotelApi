b55166da3850a69c156cc091b354a7fc8c0e70f6
kubila <dolaskyasin@gmail.com>
2021-07-09 00:02:20 +0300

    add HotelDTO

---
+++ Models/HotelDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelApi.Models
+{
+    public class HotelDTO
+    {
+        [Required]
+        [StringLength(maximumLength: 150, ErrorMessage = "Hotel name is too long!")]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(maximumLength: 200, ErrorMessage = "Address is too long!")]
+        public string Address { get; set; }
+
+        [Required]
+        [Range(1,5)]
+        public double Rating { get; set; }
+
+     
+        public int CountryId { get; set; }
+
+   
+    }
+}
