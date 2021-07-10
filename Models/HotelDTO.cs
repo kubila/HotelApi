f482ccb3bf9dcce4141c6fc330fe8b143e8af5e3
kubila <dolaskyasin@gmail.com>
2021-07-10 23:41:35 +0300

    Edit HotelDTO, add CreateHotelDTO

--- Models/HotelDTO.cs
+++ Models/HotelDTO.cs
@@ -6,23 +6,34 @@ using System.Threading.Tasks;
 
 namespace HotelApi.Models
 {
-    public class HotelDTO
+    public class CreateHotelDTO
     {
         [Required]
-        [StringLength(maximumLength: 150, ErrorMessage = "Hotel name is too long!")]
+        [StringLength(maximumLength: 150, ErrorMessage = "Hotel Name Is Too Long")]
         public string Name { get; set; }
 
         [Required]
-        [StringLength(maximumLength: 200, ErrorMessage = "Address is too long!")]
+        [StringLength(maximumLength: 250, ErrorMessage = "Address Name Is Too Long")]
         public string Address { get; set; }
 
         [Required]
-        [Range(1,5)]
+        [Range(1, 5)]
         public double Rating { get; set; }
 
-     
+        //////[Required]
         public int CountryId { get; set; }
+    }
+
+    public class HotelDTO : CreateHotelDTO
+    {
+        public int Id { get; set; }
+        public CountryDTO Country { get; set; }
+    }
+
+    public class UpdateHotelDTO : CreateHotelDTO
+    {
+
+    }
 
    
-    }
 }
